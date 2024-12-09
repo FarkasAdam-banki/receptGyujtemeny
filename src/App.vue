949927029b98b5ebbf3f9e5e0b15874f3e49a8da
Dodge0523 <csajbok.attila.csaba.2020i@bankitatabanya.hu>
2024-12-09 12:35:40 +0100

    Kereso, szuro optimal

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <script setup>
+import Kereses from './components/kereses.vue';
+
 
 </script>
 
 <template>
- 
+    <header>
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">Logo</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="#">Főoldal</a>
+                        <a class="nav-item nav-link" href="#">Receptek</a>
+                        <a class="nav-item nav-link" href="#">új recept</a>
+                        <a class="nav-item nav-link" href="#">kedvencek</a>
+                        </div>
+                    </div>
+                    </nav>
+
+    </header>  
+   <kereses />               
 </template>
 
 <style scoped>
+.navbar{
+    padding: 20px;
+}
 
 </style>
---
+++ src/components/kereses.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <section>
+    <div class="row">
+      <!-- Rövidebb kereső sáv -->
+      <input class="form-control custom-search" type="text" placeholder="Keressen receptet...">
+      
+      <div class="col-3">
+        <select class="form-select" aria-label="Minden nehézség">
+          <option selected>Minden nehézség</option>
+          <option value="1">könnyű</option>
+          <option value="2">közepes</option>
+          <option value="3">nehéz</option>
+        </select>
+      </div>
+
+      <div class="col-3">
+        <select class="form-select" aria-label="Elkészítési idő szerint">
+          <option selected>Elkészítési idő szerint</option>
+          <option value="1">név szerint</option>
+        </select>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+.custom-search {
+  width: 200px;  
+} 
+</style>
