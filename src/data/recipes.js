3fa34425ecb564d5411d9eb5829529f2249eb9e2
FarkasAdam-banki <77265777+FarkasAdam-banki@users.noreply.github.com>
2024-12-09 12:42:49 +0100

    kepek,dummy adatok meghatározása

---
+++ src/data/recipes.js
@@ -0,0 +1,46 @@
+export const recipeData = [
+    {
+      id: "1",
+      name: "Kávés sajttorta karamellöntettel",
+      diffuculty:"nehéz",
+      cooktime: 120,
+      imageurl: "../assets/kaveTorta.jpg",
+      description:"",
+
+    },
+    {
+        id: "2",
+        name: "Őzgerinceben sűlt füstös csirkemell tepsis burgonyával",
+        diffuculty:"nehéz",
+        cooktime: 60,
+        imageurl: "../assets/ozgerinc.jpg",
+        description:"",
+  
+      },
+      {
+        id: "3",
+        name: "Klasszikus tárkonyos csirkeraguleves",
+        diffuculty:"közepes",
+        cooktime: 60,
+        imageurl: "../assets/raguleves.jpg",
+        description:"",
+  
+      },{
+        id: "4",
+        name: "Spenótfőzelék fokhagymásan",
+        diffuculty:"könnyű",
+        cooktime: 30,
+        imageurl: "../assets/spenotfozelek.jpg",
+        description:"",
+  
+      },{
+        id: "5",
+        name: "Krémsajtos tojássaláta",
+        diffuculty:"könnyű",
+        cooktime: 20,
+        imageurl: "../assets/tojassalata.jpg",
+        description:"",
+  
+      }
+  ];
+  
