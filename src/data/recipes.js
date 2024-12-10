43a0da56248c5afaf6546f568866c8c37162655a
FarkasAdam-banki <77265777+FarkasAdam-banki@users.noreply.github.com>
2024-12-10 12:14:53 +0100

    dummy adatok befejezése(leirás hozzáadva) kép elérési utvonal hiba kijavitva

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -1,46 +1,46 @@
 export const recipeData = [
-    {
-      id: "1",
-      name: "Kávés sajttorta karamellöntettel",
+  {
+    id: "1",
+    name: "Kávés sajttorta karamellöntettel",
+    diffuculty:"nehéz",
+    cooktime: 120,
+    imageurl: "../assets/kaveTorta.jpg",
+    description:"A gazdag, krémes sajttorta és a kávé finom kombinációja igazi ínycsiklandó desszertet alkot. A selymes sajttorta alapot egy édes, karamellás öntet koronázza meg, így nemcsak ízben, hanem vizuálisan is lenyűgöző fogás. A desszert különleges alkalmakkor is megállja a helyét.",
+
+  },
+  {
+      id: "2",
+      name: "Őzgerinceben sűlt füstös csirkemell tepsis burgonyával",
       diffuculty:"nehéz",
-      cooktime: 120,
-      imageurl: "./assets/kaveTorta.jpg",
-      description:"",
+      cooktime: 60,
+      imageurl: "../assets/ozgerinc.jpg",
+      description:"Egy hagyományos, mégis különleges étel: a füstös csirkemell és a ropogós burgonya tökéletes párosítása. Az őzgerincben sütés során a csirke ízei egyedi füstös aromát nyernek, miközben a burgonya tökéletesen megpuhul és átveszi a fűszerek ízét. Tökéletes választás egy családi ebédhez.",
 
     },
     {
-        id: "2",
-        name: "Őzgerinceben sűlt füstös csirkemell tepsis burgonyával",
-        diffuculty:"nehéz",
-        cooktime: 60,
-        imageurl: "./assets/ozgerinc.jpg",
-        description:"",
-  
-      },
-      {
-        id: "3",
-        name: "Klasszikus tárkonyos csirkeraguleves",
-        diffuculty:"közepes",
-        cooktime: 60,
-        imageurl: "./assets/raguleves.jpg",
-        description:"",
-  
-      },{
-        id: "4",
-        name: "Spenótfőzelék fokhagymásan",
-        diffuculty:"könnyű",
-        cooktime: 30,
-        imageurl: "./assets/spenotfozelek.jpg",
-        description:"",
-  
-      },{
-        id: "5",
-        name: "Krémsajtos tojássaláta",
-        diffuculty:"könnyű",
-        cooktime: 20,
-        imageurl: "./assets/tojassalata.jpg",
-        description:"",
-  
-      }
-  ];
-  
+      id: "3",
+      name: "Klasszikus tárkonyos csirkeraguleves",
+      diffuculty:"közepes",
+      cooktime: 60,
+      imageurl: "../assets/raguleves.jpg",
+      description:"A klasszikus magyar konyha egyik elmaradhatatlan darabja. A tárkonyos csirkeraguleves friss, zöldségekkel teli, enyhén fűszeres leves, ami igazi komfortétel. Az ínycsiklandó húsleves alap és a tárkony finom aromája tökéletes harmóniában egyesül, így nemcsak a téli hónapokban, hanem bármikor nagyszerű választás.",
+
+    },{
+      id: "4",
+      name: "Spenótfőzelék fokhagymásan",
+      diffuculty:"könnyű",
+      cooktime: 30,
+      imageurl: "../assets/spenotfozelek.jpg",
+      description:"A spenótfőzelék egy könnyed, de tápláló fogás, ami a friss zöldségeket és fokhagyma intenzív ízét ötvözi. Az étel gazdag vasban és vitaminokban, miközben az ízletes fokhagyma kiemeli a spenót természetes frissességét. Ideális köret húsok mellé, de akár önálló ételként is megállja a helyét.",
+
+    },{
+      id: "5",
+      name: "Krémsajtos tojássaláta",
+      diffuculty:"könnyű",
+      cooktime: 20,
+      imageurl: "../assets/tojassalata.jpg",
+      description:"Ez a könnyed tojássaláta krémes, gazdag ízvilágot kínál, amely tökéletes választás egy gyors, mégis ínycsiklandó étkezéshez. A krémsajt lágy textúrája és a főtt tojás gazdag íze harmonikusan egyesül, miközben az apróra vágott zöldségek frissességet adnak az ételnek. Remek reggeli, uzsonna vagy köret.",
+
+    }
+];
+
