09ce04a4beaeeb655fa2ffae81c1479a4d5920ae
Dodge0523 <csajbok.attila.csaba.2020i@bankitatabanya.hu>
2024-12-16 10:02:47 +0100

    Végső háttérkép beállitása, recept.vue létrehozása és html,css felépitése az oldal fejlécének stilusbeállitásainak modositása

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,94 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import kereses from './components/kereses.vue'
-import card from './components/card.vue'
-import {recipeData} from './data/recipes.js'
+import { ref } from 'vue';
+import kereses from './components/kereses.vue';
+import recept from './components/recept.vue';
+import card from './components/card.vue';
+import { recipeData } from './data/recipes.js';
 import hozzaad from './components/hozzaad.vue';
 
+const sortedRecipes = ref([]);
+const page = ref(0);
+const info = ref(null);
 
-const szurtReceptek = ref([]);
-
-const handleNehezsegValtozas = (updatedList) => {
-  szurtReceptek.value = updatedList;
+const handleChanges = (updatedList) => {
+  sortedRecipes.value = updatedList;
 };
 
-onMounted(() => {
-  handleNehezsegValtozas(recipeData);
-});
+handleChanges(recipeData);
 
+
+const switchToAddPage = () => {
+  closeReceptDetails();
+  page.value = 1;
+  handleChanges(recipeData);
+};
+const switchToListPage = () => {
+  page.value = 0;
+};
+const showRecipeInfo = (recept) => {
+  handleChanges(recipeData);
+  info.value = recept;
+};
+const closeReceptDetails = () => {
+  info.value = null;
+};
 </script>
 
 <template>
   <div class="container-fluid">
     <header>
-                  
-                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  
-                  <a class="navbar-brand" href="#">Receptek</a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                     <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                     <ul class="navbar-nav">
-                     <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="#">Főoldal</a>
-                     </li>
-                     <li class="nav-item">
-                     <a class="nav-link" href="#">Receptek</a>
-                   </li>
-                    <li class="nav-item">
-                     <a class="nav-link" href="#">új recept</a>
-                     </li>
-                   <li class="nav-item">
-          <a class="nav-link" href="#">kedvencek</a>
-        </li>
-      </ul>
-    </div>
- 
-</nav>
-
-    </header>  
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <a class="navbar-brand" href="#">Receptkönyv</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+                @click="switchToListPage()"
+                >Receptek</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" @click="switchToAddPage()">új recept</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
     <main>
-      <kereses :elemek="recipeData"  @nehezseg-valtozas="handleNehezsegValtozas"/>
+      <div v-if="page == 0 && info == null">
+        <kereses :recipes="recipeData" @handle-change="handleChanges" />
 
-      <div class="row lista">
-        
-        <div class="col-12 col-md-6 col-lg-4 mb-4" v-for="recept in szurtReceptek" :key="recept.id">
-          <card :recept="recept" />
+        <div class="row list">
+          <div v-if="sortedRecipes.length>0" class="col-12 col-md-6 col-lg-4 mb-4"  v-for="recept in sortedRecipes"  :key="recept.id" >
+            <card :recipe="recept" @recipe-info="showRecipeInfo" />
+          </div>
+          <div v-else>
+              <p>nincs találat</p>
+          </div>
         </div>
       </div>
       <div>
-        <hozzaad />
+        <hozzaad v-if="page == 1" />
       </div>
-
+      <div v-if="info">
+        <recept :recipe="info" @close-details="closeReceptDetails" />
+      </div>
     </main>
-    <footer>
-
-
-    </footer>
   </div>
-      
-
 </template>
 
 
@@ -77,38 +97,48 @@ onMounted(() => {
 
 body {
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  background-color: #f8f9fa;
   margin: 0;
   padding: 0;
 }
 
-.lista{
-  padding: 0px 200px;
+.list{
+  padding: 0px 400px;
+}
+.navbar{
+  background-color: rgba(0, 0, 0, 0.0) !important;
 }
 header {
-  background-color: #f9f9f9;
   padding: 5px 50px;
+  background-color: rgba(0, 0, 0, 0.3);
 }
 
-.navbar {
-  border-bottom: 2px solid #f9f9f9;
-  background-color: #f9f9f9 !important;
-}
 .navbar-nav, .nav-item, .nav-link {
-  color: #000000 !important;
-  font-size: 1.1rem;
+  color: #ffffff !important;
+  font-size: 1.2rem;
   padding: 5px 10px;
+}
+
+.list p {
+  font-size: 1.7rem; 
+  font-weight: bold; 
+  color: #ff4141;
+  text-align: center; 
+  margin-top: 20px;
+  background-color: #ffbebe; 
+  padding: 10px 20px; 
+  border-radius: 8px; 
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
 }
 
 
 .navbar-brand {
   font-size: 1.5rem;
-  color: #000000 !important;
+  color: #ffffff !important;
 }
 
 
- .nav-item:hover, .nav-link:hover {
-  background-color: #a5a5a5;
+ .nav-item:hover {
+  background-color: rgba(0, 0, 0, 0.3);
   border-radius: 5px;
 }
 
@@ -137,31 +167,40 @@ input.form-control,
 select.form-select {
   margin: 10px 0;
 }
+@media (max-width: 1900px) {
+  .list{
+    padding: 0px 200px;
+  }
+}
 
 @media (max-width: 1750px) {
-  .lista{
+  .list{
     padding: 0px 100px;
   }
 }
 @media (max-width: 1500px) {
-  .lista{
+  .list{
     padding: 0px 50px;
   }
 }
 @media (max-width:950px) {
-  .lista{
+  .list{
     padding: 0px 20px;
   }
 }
 @media (max-width:500px) {
-  .lista{
+  .list{
     padding: 0px;
   }
   main{
     padding:10px 10px;
   }
 }
-
+@media (max-width:330px) {
+  .navbar-brand{
+    font-size: 20px;
+    margin: 0px;
+  }
+}
 
 </style>
-
---
+++ src/components/recept.vue
@@ -0,0 +1,203 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+    <div class="details-view">
+      <button class="close-btn">X</button>
+      
+      
+      <div class="details-card card">
+        <img class="card-img-top" :src="getURL()" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title"></h5>
+          <p class="card-text"><small class="text-body-secondary">Elkészítési idő:  perc</small></p>
+          <p class="card-text diffuculty-text">
+           
+          </p>
+        </div>
+      </div>
+      <div class="details-description">
+        <h3>Recept leírása</h3>
+        <p></p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.details-view {
+  display: flex;
+  gap: 20px;
+  padding: 20px;
+  animation: slideInScale 0.6s ease forwards;
+  position: relative;
+  flex-wrap: wrap; 
+  background-color: #eeeeee; 
+  border-radius: 10px;
+}
+
+@keyframes slideInScale {
+  0% {
+    opacity: 0;
+    transform: translateY(50px) scale(0.8);
+  }
+  60% {
+    opacity: 1;
+    transform: translateY(-5px) scale(1.03);
+  }
+  100% {
+    transform: translateY(0) scale(1);
+  }
+}
+
+@keyframes fadeOutScale {
+  0% {
+    opacity: 1;
+    transform: scale(1);
+  }
+  100% {
+    opacity: 0;
+    transform: scale(0.8);
+  }
+}
+
+.details-view.fade-out {
+  animation: fadeOutScale 0.4s ease forwards;
+}
+
+.details-view.fade-in {
+  animation: slideInScale 0.6s ease forwards;
+}
+.diffuculty-text {
+  padding: 8px 12px;
+  border-radius: 25px;
+  font-size: 1rem;
+  font-weight: bold;
+  text-transform: capitalize;
+  text-align: center; 
+  margin-top: 10px;
+}
+
+.close-btn {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+border: 1px solid #dcdcdc;
+  background: #ff5f5f;
+  color: white;
+  font-size: 1.2rem;
+  padding: 5px 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s, transform 0.2s ease;
+  width: 40px;
+  height: 40px;
+}
+
+.close-btn:hover {
+  background: #ff3b3b;
+  transform: scale(1.1);
+}
+
+.details-card {
+  flex: 1;
+  max-width: 50%;
+  background-color: #ffffff; 
+  border-radius: 15px;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.15); 
+  overflow: hidden;
+  border: 1px solid #e0e0e0; 
+}
+.card-body{
+    text-align: center;
+}
+
+.details-card img {
+  width: 100%;
+  height: auto;
+  max-height: 600px;
+}
+
+.details-description {
+    height: fit-content;
+    margin: auto 0px;
+  flex: 1;
+  font-size: 1.1rem; 
+  line-height: 1.6; 
+  overflow-y: auto; 
+  background: #ffffff;
+  border: 1px solid #dcdcdc; 
+  padding: 30px 40px;
+  background-color: #f4f4f9; 
+  border-radius: 12px;
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
+}
+
+.details-description h3 {
+  font-size: 2rem; 
+  font-weight: 600; 
+  color: #333333; 
+  margin-bottom: 15px;
+  text-align: center;
+  font-family: 'Playfair Display', serif; 
+  letter-spacing: 1px; 
+  text-transform: capitalize;
+}
+
+
+.details-description p {
+  font-size: 18px;
+  text-align: justify;
+  color: #444444;
+}
+
+.details-description p::first-letter {
+  font-size: 1.7rem; 
+  font-weight: bold;
+  color: #a37450; 
+  padding-right: 2px;
+  font-family: 'Playfair Display', serif; 
+  text-transform: capitalize;
+}
+
+.details-description img {
+  max-width: 100%; 
+  border-radius: 5px; 
+  margin-bottom: 20px; 
+}
+
+@media (max-width: 768px) {
+  .details-view {
+    flex-direction: column; 
+    gap: 10px;
+  }
+
+  .details-card {
+    max-width: 100%; 
+  }
+
+  .details-description {
+    padding: 15px; 
+  }
+
+  .close-btn {
+    font-size: 1rem; 
+    padding: 5px 8px;
+    z-index: 2;
+  }
+
+  .details-card img {
+    max-height: 400px; 
+  }
+}
+
+@media (max-width: 480px) {
+  .close-btn {
+    font-size: 0.9rem; 
+    padding: 4px 7px;
+  }
+}
+
+</style>
